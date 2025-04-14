05ee2d08e6d42d48a3359bb0db2d2e6a33e40678
shafqaat <rajashajaatali@gmail.com>
2025-04-15 02:44:34 +0500

    images path is changed

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -1,189 +1,173 @@
+// kids section
 export const kidsimages = [
-  "/src/assets/passets/kids1.jpg",
-  "/src/assets/passets/kids2.jpg",
-  "/src/assets/passets/kids3.jpg",
-  "/src/assets/passets/kids4.jpg",
-  "/src/assets/passets/kids5.jpg",
-  "/src/assets/passets/kids6.jpg",
-  "/src/assets/product1-3.jpg",
-  "/src/assets/product1-4.jpg",
-  "/src/assets/product1-5.jpg",
-  "/src/assets/product1-6.jpg",
+  "/images/passets/kids1.jpg",
+  "/images/passets/kids2.jpg",
+  "/images/passets/kids3.jpg",
+  "/images/passets/kids4.jpg",
+  "/images/passets/kids5.jpg",
+  "/images/passets/kids6.jpg",
+  "/images/product1-3.jpg",
+  "/images/product1-4.jpg",
+  "/images/product1-5.jpg",
+  "/images/product1-6.jpg",
 ];
 
+// top pakistani
 export const TopPakistani = [
-  "/src/assets/TopPakistani/tp1.jpg",
-  "/src/assets/TopPakistani/tp2.jpg",
-  "/src/assets/TopPakistani/tp3.jpg",
-  "/src/assets/TopPakistani/tp4.jpg",
-  // '/src/assets/TopPakistani/tp (5).jpg',
-  "/src/assets/TopPakistani/tp9.jpg",
-  "/src/assets/TopPakistani/tp6.jpg",
-  "../assets/TopPakistani/tp7.jpg",
-  "../assets/TopPakistani/tp8.jpg",
+  "/images/TopPakistani/tp1.jpg",
+  "/images/TopPakistani/tp2.jpg",
+  "/images/TopPakistani/tp3.jpg",
+  "/images/TopPakistani/tp4.jpg",
+  "/images/TopPakistani/tp9.jpg",
+  "/images/TopPakistani/tp6.jpg",
+  "/images/TopPakistani/tp7.jpg",
+  "/images/TopPakistani/tp8.jpg",
 ];
 
+// top games
 export const TopGames = [
-  // "/src/assets/VideoGames/vg1.jpg",
-  // "/src/assets/VideoGames/vg2.jpg",
-  // "/src/assets/VideoGames/vg3.jpg",
-  // "/src/assets/VideoGames/vg4.jpg",
-  "/public/VideoGames/vg1.jpg",
-  "/public/VideoGames/vg2.jpg",
-  "/public/VideoGames/vg3.jpg",
-  "/public/VideoGames/vg4.jpg",
-  "/public/VideoGames/vg5.jpg",
-  "/public/VideoGames/vg6.jpg",
-  // "/src/assets/VideoGames/vg5.jpg",
-  // "/src/assets/VideoGames/vg6.jpg",
-  // "/src/assets/VideoGames/vg6.jpg",
-  // "/src/assets/VideoGames/vg7.jpg",
-  // "/src/assets/VideoGames/vg8.jpg",
-  // "/src/assets/VideoGames/vg1.jpg",
-  // "/src/assets/VideoGames/vg2.jpg",
-  // "/src/assets/VideoGames/vg2.jpg",
+  "/images/VideoGames/vg1.jpg",
+  "/images/VideoGames/vg2.jpg",
+  "/images/VideoGames/vg3.jpg",
+  "/images/VideoGames/vg4.jpg",
+  "/images/VideoGames/vg5.jpg",
+  "/images/VideoGames/vg6.jpg",
 ];
 
+// general boxes
 export const GeneralboxesData = [
   {
     title: "Game Accessories",
     images: [
-      { src: "/src/assets/passets/boxSet1.jpg", label: "Chair" },
-      { src: "/src/assets/passets/boxSet3.jpg", label: "Keyboard" },
-      { src: "/src/assets/passets/boxSet2.jpg", label: "Headphones" },
-      { src: "/src/assets/passets/boxSet4.jpg", label: "Mouse" },
+      { src: "/images/passets/boxSet1.jpg", label: "Chair" },
+      { src: "/images/passets/boxSet3.jpg", label: "Keyboard" },
+      { src: "/images/passets/boxSet2.jpg", label: "Headphones" },
+      { src: "/images/passets/boxSet4.jpg", label: "Mouse" },
     ],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "Stationary, New Supplies",
-    images: [{ src: "/src/assets/passets/mainRow1.jpg" }],
+    images: [{ src: "/images/passets/mainRow1.jpg" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "Get your game on",
-    images: [{ src: "/src/assets/passets/mainRow3.jpg" }],
+    images: [{ src: "/images/passets/mainRow3.jpg" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "Shop for your Home",
     images: [
-      { src: "/src/assets/passets/bed1.jpg", label: "Chair" },
-      { src: "/src/assets/passets/bed2.jpg", label: "Keyboard" },
-      { src: "/src/assets/passets/bed3.jpg", label: "Headphones" },
-      { src: "/src/assets/passets/bed4.jpg", label: "Mouse" },
+      { src: "/images/passets/bed1.jpg", label: "Chair" },
+      { src: "/images/passets/bed2.jpg", label: "Keyboard" },
+      { src: "/images/passets/bed3.jpg", label: "Headphones" },
+      { src: "/images/passets/bed4.jpg", label: "Mouse" },
     ],
     linkText: "Discover more in Homes",
     linkUrl: "/",
   },
   {
     title: "New Year, New Supplies",
-    images: [{ src: "/src/assets/box2-1.jpg", label: "" }],
+    images: [{ src: "/images/box2-1.jpg", label: "" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "New Year, Teddy Bears",
-    images: [{ src: "/src/assets/box1-3.jpg", label: "" }],
+    images: [{ src: "/images/box1-3.jpg", label: "" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "Take Care of Pets",
-    images: [{ src: "/src/assets/box2-3.jpg", label: "" }],
+    images: [{ src: "/images/box2-3.jpg", label: "" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
   {
     title: "Latest Mobiles",
-    images: [{ src: "/src/assets/box2-2.jpg", label: "" }],
+    images: [{ src: "/images/box2-2.jpg", label: "" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
 ];
 
+// beauty boxes
 export const BeautyBoxes = [
   {
     title: "Discover Beauty Products",
     images: [
-      { src: "/src/assets/passets/Beauty (1).jpg", label: "Nails" },
-      { src: "/src/assets/passets/Beauty (2).jpg", label: "SkinCare" },
-      { src: "/src/assets/passets/Beauty (3).jpg", label: "Makeups" },
-      { src: "/src/assets/passets/Beauty (4).jpg", label: "Fragrance" },
+      { src: "/images/passets/beauty-1.jpg", label: "Nails" },
+      { src: "/images/passets/beauty-2.jpg", label: "SkinCare" },
+      { src: "/images/passets/beauty-3.jpg", label: "Makeups" },
+      { src: "/images/passets/beauty-4.jpg", label: "Fragrance" },
     ],
     linkText: "Shop More",
     linkUrl: "/",
   },
-
   {
     title: "Top Deals on Best Collection",
     images: [
-      { src: "/src/assets/passets/watch1.jpg", label: "WatchY" },
-      { src: "/src/assets/passets/watch2.jpg", label: "RolexY" },
-      { src: "/src/assets/passets/watch3.jpg", label: "StevenY" },
-      { src: "/src/assets/passets/watch4.jpg", label: "WatchY" },
+      { src: "/images/passets/watch1.jpg", label: "WatchY" },
+      { src: "/images/passets/watch2.jpg", label: "RolexY" },
+      { src: "/images/passets/watch3.jpg", label: "StevenY" },
+      { src: "/images/passets/watch4.jpg", label: "WatchY" },
     ],
     linkText: "Shop More",
     linkUrl: "/",
   },
-
   {
     title: "Gifts For Mother's Day",
-    images: [{ src: "/src/assets/passets/sb1.jpg", label: "WatchY" }],
+    images: [{ src: "/images/passets/sb1.jpg", label: "WatchY" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
-
   {
     title: "Fill Your Basket with Joy",
-    images: [{ src: "/src/assets/passets/sb2.jpg", label: "WatchY" }],
+    images: [{ src: "/images/passets/sb2.jpg", label: "WatchY" }],
     linkText: "Shop More",
     linkUrl: "/",
   },
 ];
 
-
-
+// gaming merch
 export const GamingMerchandise = [
-    {
-      title: "Gaming Merchandises",
-      images: [
-        { src: "/src/assets/passets/Beauty (1).jpg", label: "apperls" },
-        { src: "/src/assets/passets/Beauty (2).jpg", label: "hats" },
-        { src: "/src/assets/passets/Beauty (3).jpg", label: "action figuers" },
-        { src: "/src/assets/passets/Beauty (4).jpg", label: "Mugs" },
-      ],
-      linkText: "Shop More",
-      linkUrl: "/",
-    },
-  
-    {
-      title: "Toys for all Ages",
-      images: [
-        { src: "/src/assets/passets/watch1.jpg", label: "ride on|s" },
-        { src: "/src/assets/passets/watch2.jpg", label: "Buildings" },
-        { src: "/src/assets/passets/watch3.jpg", label: "Swim pools" },
-        { src: "/src/assets/passets/watch4.jpg", label: "Dolls" },
-      ],
-      linkText: "Shop More",
-      linkUrl: "/",
-    },
-  
-    {
-      title: "Gifts For Mother's Day",
-      images: [{ src: "/src/assets/passets/sb1.jpg", label: "WatchY" }],
-      linkText: "Shop More",
-      linkUrl: "/",
-    },
-  
-    {
-      title: "Fill Your Basket with Joy",
-      images: [{ src: "/src/assets/passets/sb2.jpg", label: "WatchY" }],
-      linkText: "Shop More",
-      linkUrl: "/",
-    },
-  ];
-  
+  {
+    title: "Gaming Merchandises",
+    images: [
+      { src: "/images/passets/beauty-1.jpg", label: "apperls" },
+      { src: "/images/passets/beauty-2.jpg", label: "hats" },
+      { src: "/images/passets/beauty-3.jpg", label: "action figuers" },
+      { src: "/images/passets/beauty-4.jpg", label: "Mugs" },
+    ],
+    linkText: "Shop More",
+    linkUrl: "/",
+  },
+  {
+    title: "Toys for all Ages",
+    images: [
+      { src: "/images/passets/watch1.jpg", label: "ride on|s" },
+      { src: "/images/passets/watch2.jpg", label: "Buildings" },
+      { src: "/images/passets/watch3.jpg", label: "Swim pools" },
+      { src: "/images/passets/watch4.jpg", label: "Dolls" },
+    ],
+    linkText: "Shop More",
+    linkUrl: "/",
+  },
+  {
+    title: "Gifts For Mother's Day",
+    images: [{ src: "/images/passets/sb1.jpg", label: "WatchY" }],
+    linkText: "Shop More",
+    linkUrl: "/",
+  },
+  {
+    title: "Fill Your Basket with Joy",
+    images: [{ src: "/images/passets/sb2.jpg", label: "WatchY" }],
+    linkText: "Shop More",
+    linkUrl: "/",
+  },
+];
